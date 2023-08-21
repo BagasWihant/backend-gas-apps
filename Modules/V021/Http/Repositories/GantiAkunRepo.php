b5f145f72477229d4c524c74ed218ddf461adcaa
bagaswihant <bagaswihant@gmail.com>
2023-08-21 15:29:58 +0700

    validasi [ganti akun ]

--- Modules/V021/Http/Repositories/GantiAkunRepo.php
+++ Modules/V021/Http/Repositories/GantiAkunRepo.php
@@ -2,6 +2,7 @@
 
 namespace Modules\V021\Http\Repositories;
 
+use App\Models\Marketplace\UserMarket;
 use App\Models\User;
 
 class GantiAkunRepo
@@ -9,6 +10,9 @@ class GantiAkunRepo
     public function userToStore($user)
     {
         try {
+            $market = UserMarket::where('user_id_main',$user->id)->first();
+            if(!$market) return false;
+
             User::find($user->id)->update(['as_store' => 1]);
             return true;
         } catch (\Throwable $th) {
