d925fe69cbd63212e4954538f9527fc698f74069
bagaswihant <bagaswihant@gmail.com>
2023-08-22 15:02:41 +0700

    edit [buat produk user biasa]

--- Modules/V021/Http/Controllers/Produk/ProdukController.php
+++ Modules/V021/Http/Controllers/Produk/ProdukController.php
@@ -62,41 +62,107 @@ class ProdukController extends Controller
 
         if ($user->as_store === 0) {
             $countInputProduk = count($allowed['produk']);
-
             if ($countInputProduk > 10) {
                 return new Respons(false, 'User Maks hanya 10 varian produk');
             }
-
             $countProdDB = $this->produkRepo->totalProdukVarian($user->idMarket->user_id_market);
-
             if (($countProdDB + $countInputProduk) > 10) {
                 return new Respons(false, 'User Maks hanya total 10 varian produk');
             }
         }
 
-        if ($jenis == 'F') {
+        $validatorRules = [
+            'F' => ['gender' => 'required', 'kondisi' => 'required'],
+            'K' => ['expired' => 'required|date']
+        ];
 
-            $validator2 = Validator::make($allowed, [
-                'gender' => 'required',
-                'kondisi' => 'required',
-            ]);
-            if ($validator2->fails()) return new Respons(false, 'Validation Failed', $validator2->errors());
-
-            $res = $this->produkRepo->createProdukFashion($allowed);
-
-        } elseif ($jenis == 'K') {
-
-            $validator2 = Validator::make($allowed, [
-                'expired' => 'required|date',
-            ]);
-            if ($validator2->fails()) return new Respons(false, 'Validation Failed', $validator2->errors());
-
-            $allowed['expired'] = date('Y-m-d',strtotime($allowed['expired']));
-            $res = $this->produkRepo->createProdukHarian($allowed);
-
+        $validator = Validator::make($allowed, $validatorRules[$jenis]);
+        if ($validator->fails()) {
+            return new Respons(false, 'Validation Failed', $validator->errors());
         }
-        if(!$res[0]) return new Respons(false,$res[1]);
 
-        return new Respons(true,'Berhasil memposting produk');
+        if ($jenis == 'K') {
+            $allowed['expired'] = date('Y-m-d', strtotime($allowed['expired']));
+        }
+
+        if ($user->as_store == 0) {
+            $res = $this->produkRepo->createProdukNotStore($allowed);
+        } else {
+            if ($jenis == 'F') {
+                $res = $this->produkRepo->createProdukFashion($allowed);
+            } elseif ($jenis == 'K') {
+                switch ($allowed['kategori']) {
+                    case '1':
+                        // 1.kebutuhan pokok
+                        $res = $this->produkRepo->createProdukHarian($allowed);
+                        break;
+
+                    case '2':
+                        // 2.sayur & buah
+                        $res = $this->produkRepo->createProdukSayurBuah($allowed);
+                        break;
+
+                    case '3':
+                        // 3.makan & minuman
+                        $res = $this->produkRepo->createProdukMakanMinum($allowed);
+                        break;
+
+                    case '4':
+                        // 4.bumbu dapur
+                        $res = $this->produkRepo->createProdukBumbu($allowed);
+                        break;
+
+                    case '5':
+                        // 5.perlengkapan mandi
+                        $res = $this->produkRepo->createProdukMandi($allowed);
+                        break;
+
+                    case '6':
+                        // 6.kosmetik
+                        $res = $this->produkRepo->createProdukKosmetik($allowed);
+                        break;
+
+                    default:
+                        $res = [false,'Kategori tidak ditemukan'];
+                        break;
+                }
+                // $res = $this->produkRepo->createProdukHarian($allowed);
+            }
+
+
+            switch ($jenis) {
+                case 'F':
+                    $res = $this->produkRepo->createProdukFashion($allowed);
+                    break;
+                case 'K':
+                    $kategori = $allowed['kategori'];
+                    $kategoriMethods = [
+                        '1' => 'createProdukHarian',
+                        '2' => 'createProdukSayurBuah',
+                        '3' => 'createProdukMakanMinum',
+                        '4' => 'createProdukBumbu',
+                        '5' => 'createProdukMandi',
+                        '6' => 'createProdukKosmetik'
+                    ];
+                    $res = isset($kategoriMethods[$kategori]) ?
+                        $this->produkRepo->{$kategoriMethods[$kategori]}($allowed) :
+                        [false, 'Kategori tidak ditemukan'];
+                    break;
+                default:
+                    $res = [false, 'Jenis produk tidak dikenali'];
+                    break;
+            }
+        }
+
+        return new Respons($res[0], $res[1]);
+    }
+
+    public function migrateProdukToStore(Request $req)
+    {
+        $user = $req->user();
+
+        $res = $this->produkRepo->migrateProdukToStore($user);
+
+        return new Respons($res[0], $res[1]);
     }
 }
